9fef00d93ad1fd6b69bfb58044429aa8ebc2f521
ray <vocabmonster.ir@gmail.com>
2020-10-16 19:15:15 +0330

    add solution to Distinct

---
+++ 6-Sorting/Distinct.cpp
@@ -0,0 +1,16 @@
+int solution1(vector<int> &A) {
+    vector<bool> count(2000001, 0);
+    for(size_t i=0; i<A.size(); i++)
+        count[A[i] + 1000000] = true;
+    
+    int dis = 0;
+    for(size_t i=0; i<count.size(); i++)
+        if(count[i])
+            dis++;
+            
+    return dis;
+}
+
+int solution2(vector<int> &v) {
+    return std::unordered_set<int>(v.begin(), v.end()).size();
+}
