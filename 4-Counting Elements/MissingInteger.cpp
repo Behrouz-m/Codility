4aab0aa9ccfc00bbea546660b78f29a5c63ac37a
ray <vocabmonster.ir@gmail.com>
2020-11-01 17:55:22 +0330

    update MissingInteger solution

--- 4-Counting Elements/MissingInteger.cpp
+++ 4-Counting Elements/MissingInteger.cpp
@@ -1,18 +1,12 @@
 int solution(vector<int> &A) {
-    size_t N = A.size();
-    int max_val = 1000000;
-    vector<bool> B(max_val+1, 0);
-    
-    for(size_t i=0; i<N; i++)
-    {
-        int t = A[i];
-        if(t<0)
-            t = 0;
-        B[t] = 1;
+
+    vector<bool> c(1'000'000 + 1, false);
+    for(int i : A) {
+        if(i>0)
+            c[i] = true;
     }
-    for(size_t j=1; j<B.size(); j++)
-        if(B[j] == 0)
-            return j;
-    
-    return max_val + 1;
+    for(int i=1; i<=1'000'000; i++)
+        if(c[i] == false)
+            return i;
+    return 1;
 }
