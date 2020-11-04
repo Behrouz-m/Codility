e403bc5aec6664f3c23278c4e1448b8eebfe222f
ray <vocabmonster.ir@gmail.com>
2020-11-04 20:21:54 +0330

    GCD

---
+++ 00-prewitten_algorithms/gcd.cpp
@@ -0,0 +1,16 @@
+// call it with gcd(a, b, 1) to calculate gcd(a,b)
+
+int gcd(int a, int b, int res) {
+    if (a == b)
+        return res * a;
+    else if ((a % 2 == 0) and (b % 2 == 0))
+        return gcd(a / 2, b / 2, 2 * res);
+    else if (a % 2 == 0)
+        return gcd(a / 2, b, res);
+    else if (b % 2 == 0)
+        return gcd(a, b / 2, res);
+    else if (a > b)
+        return gcd(a - b, b, res);
+    else
+        return gcd(a, b - a, res);
+}
