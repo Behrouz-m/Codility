04cdce2d7f095fba062e44ba444f5257efb899ee
ray <vocabmonster.ir@gmail.com>
2020-11-14 12:05:09 +0330

    CountTriangles

---
+++ 15-CaterpillarMethod/CountTriangles.cpp
@@ -0,0 +1,28 @@
+// result: https://app.codility.com/demo/results/training2XYSK7-EK7/
+
+#include <algorithm>
+
+int solution(vector<int> &A) {
+    if (A.size() < 3)
+        return 0;
+    int count = 0;
+    sort(A.begin(), A.end());
+    auto P = A.begin();
+
+    while (P <= A.end()-3) {
+        auto Q = P + 1;
+        auto R = Q + 1;
+        while (R < A.end()) {
+            if (*P + *Q > * R) {
+                count += R - Q;
+                R++;
+            }
+            else if (Q < R - 1)
+                Q++;
+            else
+                R++;
+        }
+        P++;
+    }
+    return count;
+}
