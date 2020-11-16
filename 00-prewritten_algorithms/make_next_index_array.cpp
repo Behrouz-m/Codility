18395320f9e2b26c2148b2e98dda86b61ff2096a
ray <vocabmonster.ir@gmail.com>
2020-11-16 12:20:03 +0330

    make_next_index_array minor edit

--- 00-prewritten_algorithms/make_next_index_array.cpp
+++ 00-prewritten_algorithms/make_next_index_array.cpp
@@ -1,10 +1,9 @@
-template<typename T>
-vector<int> getNext(vector<T>& A) {
+vector<int> getNext(vector<int>& A) {
     int n = A.size();
     vector<int> next(n+1, 0);
     next[n] = -1;
     for (int i = n - 1; i >= 0; i--)
-        if (A[i])
+        if (A[i] > 0)
             next[i] = i;
         else
             next[i] = next[i + 1];
